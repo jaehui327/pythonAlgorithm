3a60fb1694fa17c8e5dc7516d3ed2bb07a91c69b
jaehui327 <jaehui327@naver.com>
2019-12-23 23:12:52 +0900

    Add programming#09

---
+++ programming#09/geo.h
@@ -0,0 +1,5 @@
+struct point { int x, y; char c; };
+struct line { struct point p1, p2; };
+int x_value[13] = { 5, 7, 6, 8, 1, 4, 3, 2, 11, 12, 10, 9};
+int y_value[13] = { 3, 11, 12, 2, 4, 10, 6, 5, 8 ,9, 1, 7};
+char c_value[13] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'};
